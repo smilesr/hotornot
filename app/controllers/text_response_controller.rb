21064bb9b915550d0cb5be26d5284e43923a17ec
Robert Smiles Jr <smilesr120@gmail.com>
2015-08-23 16:29:56 -0400

    trying to fix why app not responding to text message

--- app/controllers/text_response_controller.rb
+++ app/controllers/text_response_controller.rb
@@ -1,10 +1,12 @@
+require 'twilio-ruby'
+
 class TextResponseController < ApplicationController
 
   def respond
       response = Twilio::TwiML::Response.new do |r|
         r.Message "Hey Monkey. Thanks for the message!"
       end
-      render_twiml response
+      twiml.text
   end
 
 end
