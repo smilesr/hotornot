f0e2b0bcb0b56eee565aae5d8155845191271232
Robert Smiles Jr <smilesr120@gmail.com>
2015-08-23 17:29:46 -0400

    add feature to lookup temp via cell phone text

--- app/controllers/weather_controller.rb
+++ app/controllers/weather_controller.rb
@@ -1,11 +1,18 @@
+require 'twilio-ruby'
 class WeatherController < ApplicationController
+  include Webhookable
 
   def get_weather
-    @state = params[:state]
-    @city = params[:city]
-    weather_key = ENV["WEATHER_KEY"]
-    @response = HTTParty.get("http://api.wunderground.com/api/#{weather_key}/conditions/q/#{@state}/#{@city}.json")
-    @temperature = @response["current_observation"]["temp_f"]
-    render
+    response = Twilio::TwiML::Response.new do |r|
+      # @state = params[:state]
+      # @city = params[:city]
+      body_request = params[:Body].split(",")
+      @city = body_request[1]
+      @state = body_request[2]
+      weather_key = ENV["WEATHER_KEY"]
+      @response = HTTParty.get("http://api.wunderground.com/api/#{weather_key}/conditions/q/#{@state}/#{@city}.json")
+      @temperature = @response["current_observation"]["temp_f"]
+    end
+    render_twiml response
   end
 end 
