1f85a26da07db7747714d25a01b0a1e9f1884e00
Robert Smiles Jr <smilesr120@gmail.com>
2015-08-23 16:33:59 -0400

    trying to fix same bug

--- app/controllers/text_response_controller.rb
+++ app/controllers/text_response_controller.rb
@@ -1,12 +1,12 @@
 require 'twilio-ruby'
 
 class TextResponseController < ApplicationController
-
+   include Webhookable
   def respond
       response = Twilio::TwiML::Response.new do |r|
         r.Message "Hey Monkey. Thanks for the message!"
       end
-      twiml.text
+      render_twiml response
   end
 
 end
