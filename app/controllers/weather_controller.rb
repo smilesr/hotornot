c8fce63f18d4336022fb9c9208ea9a487c071583
Robert Smiles Jr <smilesr120@gmail.com>
2015-08-23 20:52:17 -0400

    split weather method into multiple methods - 7th try

--- app/controllers/weather_controller.rb
+++ app/controllers/weather_controller.rb
@@ -12,45 +12,47 @@ class WeatherController < ApplicationController
   #   end
   # end
 
+#   def get_weather
+#     response = Twilio::TwiML::Response.new do |r|
+#       # @state = params[:state]
+#       # @city = params[:city]
+#       body_request = params[:Body].split(",")
+#       @city = @body_request[1]
+#       @state = @body_request[0]
+#       weather_key = ENV["WEATHER_KEY"]
+#       @response = HTTParty.get("http://api.wunderground.com/api/#{weather_key}/conditions/q/#{@state}/#{@city}.json")
+#       @temperature = @response["current_observation"]["temp_f"]
+#       send_question
+#       # r.Message "What is the current temperature in #{@city}, #{@state}"
+#     end
+#     # render_twiml response
+#   end
+
+#   def send_question
+#       response = Twilio::TwiML::Response.new do |r|
+#         r.Message "What is the current temperature in #{@city}, #{@state}"
+#       end
+#   end
+
+#   def access_answer
+#   end
+  
+
+# end 
+
   def get_weather
     response = Twilio::TwiML::Response.new do |r|
       # @state = params[:state]
       # @city = params[:city]
       body_request = params[:Body].split(",")
-      @city = @body_request[1]
-      @state = @body_request[0]
+      @city = body_request[2]
+      @state = body_request[1]
       weather_key = ENV["WEATHER_KEY"]
       @response = HTTParty.get("http://api.wunderground.com/api/#{weather_key}/conditions/q/#{@state}/#{@city}.json")
       @temperature = @response["current_observation"]["temp_f"]
-      send_question
-      # r.Message "What is the current temperature in #{@city}, #{@state}"
+      r.Message "What is the current temperature in #{@city}, #{@state}"
     end
-    # render_twiml response
+    render_twiml response
   end
 
-  def send_question
-      response = Twilio::TwiML::Response.new do |r|
-        r.Message "What is the current temperature in #{@city}, #{@state}"
-      end
-  end
-
-  def access_answer
-  end
-  
-
-end 
-
-  # def get_weather
-  #   response = Twilio::TwiML::Response.new do |r|
-  #     # @state = params[:state]
-  #     # @city = params[:city]
-  #     body_request = params[:Body].split(",")
-  #     @city = body_request[2]
-  #     @state = body_request[1]
-  #     weather_key = ENV["WEATHER_KEY"]
-  #     @response = HTTParty.get("http://api.wunderground.com/api/#{weather_key}/conditions/q/#{@state}/#{@city}.json")
-  #     @temperature = @response["current_observation"]["temp_f"]
-  #     r.Message "What is the current temperature in #{@city}, #{@state}"
-  #   end
-  #   render_twiml response
-  # end
+end
